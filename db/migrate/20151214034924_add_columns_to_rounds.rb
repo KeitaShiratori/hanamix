29aac9260660c3eae55d14abb71525dbbc78919d
Keita Shiratori <keita.shiratori@gmail.com>
2016-04-01 14:45:10 +0000

    modify db

--- db/migrate/20151214034924_add_columns_to_rounds.rb
+++ db/migrate/20151214034924_add_columns_to_rounds.rb
@@ -1,6 +1,6 @@
 class AddColumnsToRounds < ActiveRecord::Migration
   def change
-    add_column :rounds, :address, :stiring
+    add_column :rounds, :address, :string
     add_column :rounds, :latitude, :float
     add_column :rounds, :longitude, :float
   end
